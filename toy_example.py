db29f8be28b52a5819939fcf0ee6d69b00e605b9
Felipe Curcio <felipe_nieto010@hotmail.com>
2023-12-01 01:05:00 -0300

    add threading lock toy example

---
+++ toy_example.py
@@ -0,0 +1,46 @@
+import threading
+import time
+
+class A:
+    def __init__(self):
+        self.lock = threading.Lock()
+
+    def anotherDecorator(f):
+        def wrapper(*args, **kwargs):
+            f(*args, **kwargs)
+        return wrapper
+
+    def someFn(f):
+        def wrapper(*args, **kwargs):
+            f(*args, **kwargs)
+        return wrapper
+
+    @anotherDecorator
+    @someFn
+    def fn(self):
+        self.lock.acquire()
+        print("ESTOU EM FN")
+        time.sleep(15)
+        self.lock.release()
+    
+    @anotherDecorator
+    @someFn
+    def fn2(self):
+        self.lock.acquire()
+        print("ESTOU EM FN2")
+        self.lock.release()
+
+    @anotherDecorator
+    @someFn
+    def fn3(self):
+        print("ESTOU EM FN3")
+    
+a = A()
+th = threading.Thread(target=a.fn, name='dale1')
+th2 = threading.Thread(target=a.fn2, name='dale2')
+th2 = threading.Thread(target=a.fn3, name='dale3')
+th.start()
+th2.start()
+
+th.join()
+th2.join()
