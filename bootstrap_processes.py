4581ee670f9ef55d1c80ab9a08b776ada86a96ca
Felipe Curcio <felipe_nieto010@hotmail.com>
2023-12-02 17:09:34 -0300

    bootstra_processes - erase file content

--- bootstrap_processes.py
+++ bootstrap_processes.py
@@ -1,12 +1,19 @@
 import multiprocessing
 import subprocess
+from process import FILENAME
 
 N_PROCESSES = 4
 
 def run_process():
     subprocess.run(["python", "process.py"])
 
+def erase_content_from_critical_section_file():
+    with open(FILENAME, "w") as file:
+        file.write("")
+
 if __name__ == "__main__":
+    erase_content_from_critical_section_file()
+
     processes = []
 
     for _ in range(N_PROCESSES):
