e7b08c8ae840fd701994ad2e7bf7f6b073e0820a
Felipe Curcio <felipe_nieto010@hotmail.com>
2023-12-02 17:31:11 -0300

    update toy_example

--- toy_example.py
+++ toy_example.py
@@ -1,47 +1,27 @@
 import threading
 import time
 
+WAIT_TIME = 5
+
 class A:
     def __init__(self):
         self.lock = threading.Lock()
 
-    def anotherDecorator(f):
-        def wrapper(*args, **kwargs):
-            f(*args, **kwargs)
-        return wrapper
-
-    def someFn(f):
-        def wrapper(*args, **kwargs):
-            f(*args, **kwargs)
-        return wrapper
-
-    @anotherDecorator
-    @someFn
-    def fn(self):
+    def fn(self, text):
+        print(f"FN: {text} - LOCKED? {self.lock.locked()}" )
         self.lock.acquire()
-        print("ESTOU EM FN")
-        time.sleep(5)
-        self.lock.release()
-    
-    @anotherDecorator
-    @someFn
-    def fn2(self):
-        self.lock.acquire()
-        print("ESTOU EM FN2")
+        print(F"fn: {text} - waiting {WAIT_TIME} secs")
+        time.sleep(WAIT_TIME)
         self.lock.release()
 
-    @anotherDecorator
-    @someFn
-    def fn3(self):
-        print("ESTOU EM FN3")
-    
 a = A()
-th = threading.Thread(target=a.fn, name='dale1')
-th2 = threading.Thread(target=a.fn2, name='dale2')
-th3 = threading.Thread(target=a.fn3, name='dale3')
+th = threading.Thread(target=a.fn, args=('first-call',), name='first-call')
+th2 = threading.Thread(target=a.fn, args=('second-call',), name='second-call')
+th3 = threading.Thread(target=a.fn, args=('third-call',), name='third-call')
+
 th.start()
-th2.start()
 th3.start()
+th2.start()
 
 th.join()
 th2.join()
